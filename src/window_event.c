da221d02b44ce32a0ed9907cdf6dceb76fda3bca
vkuksa <vkuksa@student.unit.ua>
2018-06-30 13:47:05 +0300

    fixed issue when work group size dropped due to window resize

--- src/window_event.c
+++ src/window_event.c
@@ -27,20 +27,38 @@ void	cl_free(t_cldata *cl)
 	clReleaseMemObject(cl->obj_gpu);
 }
 
+void	handle_resize(t_env *env)
+{
+	env->sc.cam.pr_pl_w = g_win_width;
+	env->sc.cam.pr_pl_h = g_win_height;
+	cl_free(&env->cl);
+	cl_setup(env);
+	get_work_group_size(&env->cl);
+	printf("width: %u; height: %u\n", g_win_width, g_win_height);
+	printf("local size: %lu\n", env->cl.local_size);
+	printf("tututu0\n");
+	env->num_samples = 0;
+	printf("tututu0\n");
+	SDL_FreeSurface(env->screen.surface);
+	printf("tututu2\n");
+	env->screen.surface = SDL_GetWindowSurface(env->screen.window);
+	printf("tututu3\n");
+	env->screen.surf_arr = (t_rgb *)env->screen.surface->pixels;
+}
+
+/*
+**	if the work group is too small (render speed drops rapidly)
+**	the window gets one pixel wider
+*/
+
 void	window_event(SDL_Event e, t_env *env)
 {
 	if (e.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
 	{
 		g_win_width = (unsigned int)e.window.data1;
 		g_win_height = (unsigned int)e.window.data2;
-		env->sc.cam.pr_pl_w = g_win_width;
-		env->sc.cam.pr_pl_h = g_win_height;
-		cl_free(&env->cl);
-		cl_setup(env);
-		get_work_group_size(&env->cl);
-		env->num_samples = 0;
-		SDL_FreeSurface(env->screen.surface);
-		env->screen.surface = SDL_GetWindowSurface(env->screen.window);
-		env->screen.surf_arr = (t_rgb *)env->screen.surface->pixels;
+		handle_resize(env);
+		if (env->cl.local_size < 50)
+			SDL_SetWindowSize(env->screen.window, ++g_win_width, g_win_height);
 	}
 }
